dcde8c5f5f6c324bb8c5beba30e1a5e1cc869c6f
unknown <selmirlokmic@gmailc.com>
2025-05-30 16:02:05 +0200

    test done

--- CallDetailRecord/Program.cs
+++ CallDetailRecord/Program.cs
@@ -8,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Dependency injection
 builder.Services.AddScoped<CDRService>();
 
 var app = builder.Build();

ddaf7c9015987b5772fe5e5a6876a9ac2553ff3b
Selmir Lokmic <selmirlokmic@gmail.com>
2025-06-03 09:49:45 +0200

    Clean up tracked files now ignored by .gitignore

--- CallDetailRecord/Controllers/OrganizationController.cs
+++ CallDetailRecord/Controllers/OrganizationController.cs
@@ -1,7 +1,10 @@
-﻿using CallDetailRecord.Models;
+﻿using CallDetailRecord.Data;
+using CallDetailRecord.Models;
 using CallDetailRecord.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace CallDetailRecord.Controllers
 {
@@ -9,30 +12,78 @@ namespace CallDetailRecord.Controllers
     [ApiController]
     public class OrganizationController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public OrganizationController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public async Task<ActionResult<List<Organization>>> GetAllOrganizations()
         {
-            var org = new List<Organization> {
-                new Organization
-                {
-                    OrganizationId = 1,
-                    Name = "Test org",
-                    Adress = "Test adress",
-                    Email = "Test@gmail.com",
-                    PhoneNumber = "41088447",
-                    PostCode = 5135
-                },
-                new Organization
-                {
-                    OrganizationId = 2,
-                    Name = "Test org2",
-                    Adress = "Test adress2",
-                    Email = "Test2@gmail.com",
-                    PhoneNumber = "41088448",
-                    PostCode = 5135
-                }
-            };
-            return Ok(org);
+            var organizations = await _context.Organizations.ToListAsync();
+            return Ok(organizations);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Organization>> GetOrganization(int id)
+        {
+            var organization = await _context.Organizations.FindAsync(id);
+            if(organization is null)
+            {
+                return NotFound("Organization not found!");
+            }
+            return Ok(organization);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Organization>> CreateOrganization(Organization organization)
+        {
+            if (_context.Organizations.Any(o => o.Email == organization.Email))
+            {
+                return BadRequest("Email er allerede i bruk.");
+            }
+
+            _context.Organizations.Add(organization);
+            await _context.SaveChangesAsync();
+
+            return Ok(organization);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Organization>> UpdateOrganization(Organization organization)
+        {
+            var dbOrg = await _context.Organizations.FindAsync(organization.OrganizationId);
+            if (dbOrg is null)
+            {
+                return NotFound("Organization not found!");
+            }
+
+            dbOrg.PhoneNumber = organization.PhoneNumber;
+            dbOrg.Email = organization.Email;
+            dbOrg.Adress = organization.Adress;
+            dbOrg.PostCode = organization.PostCode;
+            dbOrg.Name = organization.Name;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(organization);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult> DeleteOrganization(int id)
+        {
+            var dbOrg = await _context.Organizations.FindAsync(id);
+            if (dbOrg is null)
+            {
+                return NotFound("Organization not found!");
+            }
+
+            _context.Organizations.Remove(dbOrg);
+
+            await _context.SaveChangesAsync();
+
+            return Ok();
         }
     }
 }
